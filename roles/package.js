2d85720b8d2f96287ab4438c0b3674c0247f86fe
Spencer Lambert <spence@wisptools.com>
2015-05-07 20:45:38 -0600

    Adding mongo to the dependency list

--- roles/package.js
+++ roles/package.js
@@ -8,12 +8,12 @@ Package.describe({
 Package.on_use(function (api) {
   api.versionsFrom && api.versionsFrom("METEOR@0.9.0");
   var both = ['client', 'server'];
-  api.use(['underscore', 'accounts-base'], both);
+  api.use(['underscore', 'accounts-base', 'mongo'], both);
   api.use(['handlebars'], 'client', {weak: true});
 
   // This is needed due to Meteor Issue #1358
   //   https://github.com/meteor/meteor/issues/1358
-  // The 'weak' flag doesn't work with packages that aren't 
+  // The 'weak' flag doesn't work with packages that aren't
   // in meteor's internal cache (ie. non-core packages)
   if(uiExists()) {
     api.use(['ui'], 'client', {weak: true});
@@ -66,7 +66,7 @@ function uiExists() {
   //if (/^\s*standard-app-packages\s*$/m.test(meteorPackages)) {
     // The ui package may or may _not_ be there...
     // Releases before 0.8.0 had standard-app-packages but not
-    // ui.  Without weak references working properly, there is 
+    // ui.  Without weak references working properly, there is
     // no good way to detect the inclusion of the ui package in
     // bundled apps.
   //}
